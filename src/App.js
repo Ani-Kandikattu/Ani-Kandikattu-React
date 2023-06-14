e7de6e78b512f26c03eb1c84f4e87ffe24d882d0
Ani-Kandikattu <anikandikattu@gmail.com>
2023-06-14 15:21:23 -0700

    added components and basic code for each component and imported them in App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React from "react";
 import './index.css'
+import Header from './components/Header';
+import About from './components/About';
+import Contact from './components/Contact';
+import Portfolio from './components/Portfolio';
+import Resume from './components/Resume';
+import Footer from './components/Footer';
 
 function App() {
   return (
---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function about() {
+  return (
+    <div>
+    <h1>Full Stack Software Developer</h1>
+    <p></p>
+    </div>
+  )
+}
